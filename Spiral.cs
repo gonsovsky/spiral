7e832472237fd1ab05d9e74b1f65797f9e4b3e37
Konstantin Gonsovsky <k.gonsovsky@yandex.ru>
2020-07-15 18:58:15 +0300

    first commit

---
+++ Spiral.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiralTest
+{
+    public class Spiral
+    {
+        public static IEnumerable<int> Decode(int[,] a)
+        {
+            int i, k = 0, l = 0;
+            var n = a.GetLength(1);
+            var m = a.GetLength(0);
+            while (k < m && l < n)
+            {
+                for (i = l; i < n; ++i)
+                    yield return a[k, i];
+                k++;
+                for (i = k; i < m; ++i)
+                    yield return a[i, n - 1];
+                n--;
+                if (k < m)
+                {
+                    for (i = n - 1; i >= l; --i)
+                        yield return a[m - 1, i];
+                    m--;
+                }
+                if (l < n)
+                {
+                    for (i = m - 1; i >= k; --i)
+                        yield return a[i, l];
+                    l++;
+                }
+            }
+        }
+
+        static public int[,] Encode(int[] vec)
+        {
+            int idx = 0;
+            var m = (int)Math.Ceiling(Math.Sqrt(vec.Count()));
+            var n = m;
+            var a = new int[m, n];
+            int k = 0, l = 0;
+            while (k < m && l < n)
+            {
+                for (int i = l; i < n; ++i)
+                    a[k, i] = vec[idx++];
+                k++;
+                for (int i = k; i < m; ++i)
+                    a[i, n - 1] = vec[idx++];
+                n--;
+                if (k < m)
+                { 
+                    for (int i = n - 1; i >= l; --i)
+                        a[m - 1, i] = vec[idx++];
+                    m--;
+                }
+                if (l < n)
+                {
+                    for (int i = m - 1; i >= k; --i)
+                        a[i, l] = vec[idx++];
+                    l++;
+                }
+            }
+            return a;
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpiralTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        [TestMethod]
+        public void SquareDecode()
+        {
+            int[,] inputMatrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            var wantVector = new int[] { 1, 2, 3, 6, 9, 8, 7, 4, 5 };
+            var outputVector = Spiral.Decode(inputMatrix).ToArray();
+
+            var ok = Enumerable.SequenceEqual(outputVector, wantVector);
+            Assert.AreEqual(ok, true);
+        }
+
+        [TestMethod]
+        public void SquareEncode()
+        {
+            int[] inputVector = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[,] wantMatrix = { { 1, 2, 3 }, { 8, 9, 4 }, { 7, 6, 5 } };
+            var outputMatrix = Spiral.Encode(inputVector);
+
+            var ok =
+                wantMatrix.Rank == outputMatrix.Rank &&
+                Enumerable.Range(0, wantMatrix.Rank).All(dimension => wantMatrix.GetLength(dimension) == outputMatrix.GetLength(dimension)) &&
+                wantMatrix.Cast<int>().SequenceEqual(outputMatrix.Cast<int>());
+            Assert.AreEqual(ok, true);
+        }
+
+
+        [TestMethod]
+        public void NearlySquareDecode()
+        {
+            int[,] inputMatrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+            var wantVector = new int[] { 1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7 };
+            var outputVector = Spiral.Decode(inputMatrix).ToArray();
+
+            var ok = Enumerable.SequenceEqual(outputVector, wantVector);
+            Assert.AreEqual(ok, true);
+        }
+
+
+        //This test will fail :(
+        [TestMethod]
+        public void NearlySquareEncode()
+        {
+            int[] inputVector = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            int[,] wantMatrix = { { 1, 2, 3, 4 }, { 10, 11, 12, 5 }, { 9, 8, 7, 6 } };
+            var outputMatrix = Spiral.Encode(inputVector);
+
+            var ok =
+                wantMatrix.Rank == outputMatrix.Rank &&
+                Enumerable.Range(0, wantMatrix.Rank).All(dimension => wantMatrix.GetLength(dimension) == outputMatrix.GetLength(dimension)) &&
+                wantMatrix.Cast<int>().SequenceEqual(outputMatrix.Cast<int>());
+           
+            Assert.AreEqual(ok, true);
+        }
+    }
+}
